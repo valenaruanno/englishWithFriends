ddc079e49045c337a97993a32ca1dd58563247da
valenaruanno <valentinaruanno2004@gmail.com>
2025-12-11 16:22:59 -0300

    Arreglo el problema de java not found en el servidor

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: cd englishteacher-api && java -Dserver.port=$PORT -jar target/englishteacher-api-0.0.1-SNAPSHOT.jar
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd englishteacher-api
 chmod +x mvnw
-./mvnw clean package -DskipTests
-java -jar target/englishteacher-api-0.0.1-SNAPSHOT.jar
+./mvnw clean package -DskipTests -Dmaven.compiler.release=21
+java -Dserver.port=${PORT:-8080} -jar target/englishteacher-api-0.0.1-SNAPSHOT.jar
